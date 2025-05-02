cd0e25edd47593cc2b0fef800e3a21c554c523ef
chino4242 <ryanj.contino@gmail.com>
2025-05-02 10:57:56 -0400

    Update message

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className = "App">
       <header className ="App-header">
-        <h1>Hello World from Volatile Creative.</h1>
+        <h1>Hello Mala: from Volatile Creative (AKA Papo)</h1>
         <p>Message from backend: {message || 'Loading...'}</p>
       </header>
     </div>
