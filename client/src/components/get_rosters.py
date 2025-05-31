7f9d737af97f718c77f2c1a77ef3888e1c1a4495
chino4242 <ryanj.contino@gmail.com>
2025-05-30 20:56:46 -0400

    Creates dataframe per team roster

--- client/src/components/get_rosters.py
+++ client/src/components/get_rosters.py
@@ -1,6 +1,6 @@
 import sys
 import os
-import pandas
+import pandas as pd
 import requests
 import json
 
@@ -26,6 +26,36 @@ user_url = "https://api.sleeper.app/v1/user/{user_id}"
 rosters = requests.get(roster_url)
 managers = requests.get(managers_url)
 
+def create_manager_roster(manager_player_ids, all_players_data):
+    roster_details_list = []
+    for player_id_on_roster in manager_player_ids:
+        player_id_str = str(player_id_on_roster)
+        player_info = all_players_data.get(player_id_on_roster)
+        
+        if player_info:
+            roster_details_list.append({
+                'player_id': player_id_str,
+                'full_name': player_info.get('full_name', 'N/A'),
+                'position': player_info.get('position', 'N/A'),
+                'team': player_info.get('team', 'FA')
+            })
+        else:
+            if len(player_id_str) <= 3 and player_id_str.isalpha() and player_id_str.isupper():
+                roster_details_list.append({
+                'player_id': player_id_str,
+                'full_name': f"{player_id_str} Defense",
+                'position': 'DEF',
+                'team': player_id_str
+                })
+            else:
+                roster_details_list.append({
+                    'player_id': player_id_str,
+                    'full_name': 'Unknown Player/Entity',
+                    'position': 'N/A',
+                    'team': 'N/A'
+                })
+    manager_df = pd.DataFrame(roster_details_list)
+    return manager_df
 
 roster_data = rosters.json()
 managers_data = managers.json()
@@ -34,11 +64,12 @@ if not roster_data or not managers_data:
     sys.exit(1)
 
 for roster in roster_data:
-   print(roster['owner_id'])
+   players = roster['players']
+   players_details = create_manager_roster(players, all_players_data)
    user = requests.get(user_url.format(user_id=roster['owner_id']))
    user_data = user.json()
-   print(user_data)
-   print(roster['players'])
+   print(user_data['display_name'])
+   print(players_details)
    print("-------------------")
    
 for manager in managers_data:
