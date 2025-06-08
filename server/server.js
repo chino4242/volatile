a93bebf2695754d7fb83bb7517dc2e517d6b18e3
chino4242 <ryanj.contino@gmail.com>
2025-06-08 08:48:32 -0400

    Remove path variable

--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ process.on('uncaughtException', (error) => {
 
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 
 // vvvv KEEP THIS COMMENTED OUT vvvv
 const rosterRoutes = require('./routes/rosterRoutes'); 
@@ -24,8 +23,6 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-
 
 app.use('/api', rosterRoutes);
 
