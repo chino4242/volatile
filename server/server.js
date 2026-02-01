475690013b404a060f95eccdae7a0beabac8da84
chino4242 <ryanj.contino@gmail.com>
2026-02-01 15:50:38 -0500

    fix: manual CORS headers in server.js

--- server/server.js
+++ server/server.js
@@ -26,6 +26,19 @@ const enrichedPlayerRoutes = require('./routes/enrichedPlayerRoutes'); // Import
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// MANUAL CORS MIDDLEWARE - Force headers to avoid issues with aws-serverless-express/cors interaction
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+    // Intercept OPTIONS method
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 // Update the request logging middleware
 app.use((req, res, next) => {
     const requestLog = {
