2e4aa27ea811ab8d13dd8bfe6ad0b7bd5834573b
chino4242 <ryanj.contino@gmail.com>
2026-02-16 20:21:24 -0500

    Add TypeScript declaration for resource.js

---
+++ amplify/functions/api-server/resource.d.ts
@@ -0,0 +1,3 @@
+import { ConstructFactory, ResourceProvider, FunctionResources } from '@aws-amplify/backend';
+
+export declare const apiServer: ConstructFactory<ResourceProvider<FunctionResources>>;
